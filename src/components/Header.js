9fed6e79571e69d9a7a4861744ab5f00bddc7bcd
josiah <josiahdamiwilliams@gmail.com>
2019-11-15 18:11:50 +0100

    New change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import styled from "styled-components";
 
 
 export default function Header() {
-  const Heading = styled.h1`
+  const Heading = styled.div`
   font-size: 20px;
   text-shadow: 2px 2px BlueViolet;
   text-align: center;
 `
   return (
     <Heading className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <h2 className="ui center">Rick &amp; Morty Fan Page</h2>
     </Heading>
   );
 }
